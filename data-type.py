c4d123ec5d30eb08a48f95cb5dd4891a56e63105
Lucas0303-art <sunakim.marie@gmail.com>
2024-09-27 20:57:34 -0500

    data 추가

---
+++ data-type.py
@@ -0,0 +1,9 @@
+a=10
+b=1.1
+c='python'
+print(type(a),type(b),type(c))
+#int! u can store integer number
+
+#float  can store real numbers
+
+#string u can save text
--- input.py
+++ input.py
@@ -1,7 +1,17 @@
-print('hi')
+#print('hi')
 
-a='hi'
-print(a)
+#a='hi'
+#print(a)
 
-a=input('type:')
+#a=input('type:')
+#print(a)
+
+a=42
 print(a)
+print(a+5)
+
+
+
+b=input('type in number')
+print(b)
+print(b+5)
