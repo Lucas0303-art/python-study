0ee42a6ae1adbb06b769e244a3a358902695baf6
Lucas0303-art <sunakim.marie@gmail.com>
2024-12-06 18:43:43 -0600

    baek1330 추가 및 풀이

---
+++ baekjoon/baek1330.py
@@ -0,0 +1,19 @@
+# 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+# input : 10 20
+# output : <
+# input : 15 10
+# output : >
+# input : 10 10
+# output : =
+
+a = int(input('type in number'))
+b = int(input('type in number'))
+
+if a>b:
+    print('>')
+
+if a<b:
+    print('<')
+
+if a==b:
+    print('=')
